c8f0d5f2476e682a558e068b37197fcaeaa5ad9c
SILVESTRE Milan <milansilvestre@umontpellier.fr>
2023-08-11 14:50:19 +0200

    5000000 steps results

--- KMC_Spyder/SiGrowth/Travail sur les évènements/run_test_4_steps_A_B.py
+++ KMC_Spyder/SiGrowth/Travail sur les évènements/run_test_4_steps_A_B.py
@@ -97,8 +97,8 @@ model = KMCLatticeModel(configuration=config,
 # a seed value will result in the wall clock time seeding,
 # so we would expect slightly different results each time
 # we run this test.
-control_parameters = KMCControlParameters(number_of_steps=100000,
-                                          dump_interval=10000,
+control_parameters = KMCControlParameters(number_of_steps=5000000,
+                                          dump_interval=500000,
                                           seed=120)
 
 model.run(control_parameters, trajectory_filename="custom_traj_4_steps.py")
