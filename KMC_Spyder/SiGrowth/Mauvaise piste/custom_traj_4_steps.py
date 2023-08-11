fbe59cf46aba22fecad6078f2b6ade21a4c7f745
MSilvestre-Growth <141043947+MSilvestre-Growth@users.noreply.github.com>
2023-08-11 10:29:02 +0200

    SiGrowth with the good events

--- KMC_Spyder/SiGrowth/Mauvaise piste/custom_traj_4_steps.py
+++ KMC_Spyder/SiGrowth/Mauvaise piste/custom_traj_4_steps.py
@@ -17891,3 +17891,32 @@ types.append(["A3","B4","B4","A3","B4","B2","B2","B2","A1","B2","A1","A3","B2","
               "A3","A1","B2","B4","B2","A1","B2","B2","B2","A3","A1","B4","B2","A3",
               "B2","B2","B4","A3","A3","B2","A1","A1","B2","B4","B2","B2","B2","B2",
               "A3","B4","A3","B2"])
+
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import islice
+from PIL import Image
+
+possible_types = ['A1','B2','A3','B4']
+
+for i in range(len(types)):
+    KMC_Result_current = types[i]
+    # Conversion of "U" in 1 and "D" in 0 for display purposes
+    for j in range(len(KMC_Result_current)):
+        for k in range(len(possible_types)):
+            if KMC_Result_current[j] == possible_types[k]:
+                KMC_Result_current[j] = k/len(possible_types)
+    
+    # Using islice to turn KMC_Result_current (list) in KMC_Result_current_matrix (100x100 matrix)
+    length_to_split = 100 * np.ones(100)
+    KMC_Result_current = iter(KMC_Result_current)
+    KMC_Result_current_matrix = [list(islice(KMC_Result_current, int(elem))) for elem in length_to_split]
+    KMC_Result_current_matrix = np.array(KMC_Result_current_matrix)
+    
+    #Image display
+    #plt.figure()
+    #plt.imshow(KMC_Result_current_matrix)
+    
+    #Image saving in other directory
+    plt.imsave("..\..\..\Images\SiGrowth\steps_4_test\Imtest%d.png" % i, KMC_Result_current_matrix)
+ 
---
+++ KMC_Spyder/SiGrowth/Travail sur les évènements/custom_processes_An_Bn.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 16:52:10 2023
+
+@author: msilvestre
+"""
+
+# Copyright (c)  2014  Mikael Leetmaa
+#
+# This file is part of the KMCLib project distributed under the terms of the
+# GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
+#
+
+
+from KMCLib import *
+
+# store the configuration to get all possibles types
+config = KMCConfigurationFromScript("config_4_steps.py")
+
+coordinates = [[   0.000000e+00,   0.000000e+00,   0.000000e+00]]
+processes = []
+
+# Generic process = add a quasi-dimere on top of a layer, or remove a quasi-dimere
+# from the top of a layer --> this has to be done for all the steps stored in config
+
+# Get the possible types from config
+dictionnary_of_possible_types = config.possibleTypes()
+#print dictionnary_of_possible_types
+# Removal of generic '*' type from the dictionnary
+del dictionnary_of_possible_types['*']
+# Conversion of a dictionnary in the list of all entries
+list_of_possible_types = dictionnary_of_possible_types.keys()
+#print list_of_possible_types
+
+processes = ['no event']*2*(len(list_of_possible_types))
+
+# Get min and max height to respect types with the periodicity
+max_height = int(list_of_possible_types[0][1])
+type_of_highest_step = list_of_possible_types[0][0]
+
+min_height = int(list_of_possible_types[0][1])
+type_of_lowest_step = list_of_possible_types[0][0]
+
+for i in range(len(list_of_possible_types)):
+    current_height = int(list_of_possible_types[i][1])
+    if current_height > max_height:
+        max_height = current_height
+        type_of_highest_step = list_of_possible_types[i][0]
+        
+    if current_height < min_height:
+        min_height = current_height
+        type_of_lowest_step = list_of_possible_types[i][0]
+
+#print 'List of all processes :'
+# Set each individual process in the processes list
+for i in range(len(list_of_possible_types)):
+    
+    current_step = list_of_possible_types[i]
+    current_type_of_step = current_step[0]
+    #print "current type of step = " + current_type_of_step
+    current_height = int(current_step[1])
+    
+    if current_type_of_step == 'A':
+        next_step_type = 'B'
+    else:
+        next_step_type = 'A'
+    
+    # Add a quasi-dimere on top of the current step
+    
+        # if statement to respect periodicity
+    if current_height == max_height:
+        next_height = str(min_height)
+        next_step_type = type_of_lowest_step
+    else:
+        next_height = str(current_height + 1)
+    
+        # Name of the new step
+    next_step = next_step_type + next_height
+    
+    print 'Add a dimere on top of the current layer ' + current_step + ' --> ' + next_step
+    processes[2*(current_height-1)] = KMCProcess(coordinates=coordinates,
+                                           elements_before=[current_step],
+                                           elements_after=[next_step],
+                                           basis_sites=[0],
+                                           rate_constant=1.0)
+    
+    # Remove a quasi-dimere on top of the current step
+    
+    if current_type_of_step == 'A':
+        next_step_type = 'B'
+    else:
+        next_step_type = 'A'
+    
+        # if statement to respect periodicity
+    if current_height == min_height:
+        next_height = str(max_height)
+        next_step_type = type_of_highest_step
+    else:
+        next_height = str(current_height - 1)
+        
+        # Name of the new step
+    next_step = next_step_type + next_height
+    
+    print 'Remove a dimere on top of the current layer ' + current_step + ' --> ' + next_step
+    processes[2*(current_height-1)+1] = KMCProcess(coordinates=coordinates,
+                                               elements_before=[current_step],
+                                               elements_after=[next_step],
+                                               basis_sites=[0],
+                                               rate_constant=1.0)
+
+# Create the interactions object.
+# number of one process = initial height of the targeted atom + X
+# X = 0 if the process add one dimere on top, X = 1 if it remove the dimere
+print processes
+interactions = KMCInteractions(processes, implicit_wildcards=True)
