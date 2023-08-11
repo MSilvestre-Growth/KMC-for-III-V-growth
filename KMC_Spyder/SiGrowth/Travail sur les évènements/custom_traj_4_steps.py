cabebb8c36e036c2a3b6dcf2d0dfe2b95b6043ca
MSilvestre-Growth <141043947+MSilvestre-Growth@users.noreply.github.com>
2023-08-11 10:50:59 +0200

    first tests seem ok

--- KMC_Spyder/SiGrowth/Travail sur les évènements/custom_traj_4_steps.py
+++ KMC_Spyder/SiGrowth/Travail sur les évènements/custom_traj_4_steps.py
@@ -15823,3 +15823,32 @@ types.append(["A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A
               "B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B",
               "B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B",
               "B","B","B","B","B","B"])
+
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import islice
+from PIL import Image
+
+possible_types = ['A','B']
+
+for i in range(len(types)):
+    KMC_Result_current = types[i]
+    # Conversion of "U" in 1 and "D" in 0 for display purposes
+    for j in range(len(KMC_Result_current)):
+        for k in range(len(possible_types)):
+            if KMC_Result_current[j] == possible_types[k]:
+                KMC_Result_current[j] = k/len(possible_types)
+    
+    # Using islice to turn KMC_Result_current (list) in KMC_Result_current_matrix (100x100 matrix)
+    length_to_split = 30 * np.ones(30)
+    KMC_Result_current = iter(KMC_Result_current)
+    KMC_Result_current_matrix = [list(islice(KMC_Result_current, int(elem))) for elem in length_to_split]
+    KMC_Result_current_matrix = np.array(KMC_Result_current_matrix)
+    
+    #Image display
+    #plt.figure()
+    #plt.imshow(KMC_Result_current_matrix)
+    
+    #Image saving in other directory
+    plt.imsave("..\..\..\Images\SiGrowth\steps_4_test\Imtest%d.png" % i, KMC_Result_current_matrix)
+    
