2a3f40248dd9a3cfeed4553b43be3cb81829c129
MSilvestre-Growth <141043947+MSilvestre-Growth@users.noreply.github.com>
2023-08-16 11:33:08 +0200

    prise en compte des sauts de marches

--- KMC_Spyder/SiGrowth/Travail sur les évènements/custom_processes_An_Bn_bon_niveaux.py
+++ KMC_Spyder/SiGrowth/Travail sur les évènements/custom_processes_An_Bn_bon_niveaux.py
@@ -58,7 +58,7 @@ list_of_coordinates = [[[0.0, 0.0, 0.0], [1.0, 0.0, 0.0]],
                        [[0.0, 0.0, 0.0], [0.0, -1.0, 0.0]]
                        ]
 
-for a in range(len(sorted_list_of_possible_types)-1):
+for a in range(len(sorted_list_of_possible_types)-2):
 
     #################################################
     #        Deposition of a quasi-dimere           #
@@ -83,6 +83,7 @@ for a in range(len(sorted_list_of_possible_types)-1):
     #################################################
     #         Diffusion of a quasi-dimere           #
     #################################################
+    step_jump = sorted_list_of_possible_types[a+2]
     
     before_moving = [elements_after, elements_before]
     #print 'before_moving'
@@ -92,37 +93,56 @@ for a in range(len(sorted_list_of_possible_types)-1):
     #print 'after_moving'
     #print after_moving
     
+    step_jumping = [elements_before, step_jump]
+    
     for i in range(len(list_of_coordinates)):
         processes.append(KMCProcess(coordinates=list_of_coordinates[i],
                                                elements_before=before_moving,
                                                elements_after=after_moving,
                                                basis_sites=[0],
                                                rate_constant=0.0))
+    for i in range(len(list_of_coordinates)):   
+        processes.append(KMCProcess(coordinates=list_of_coordinates[i],
+                                               elements_before=before_moving,
+                                               elements_after=step_jumping,
+                                               basis_sites=[0],
+                                               rate_constant=0.0))
     
 
 
-# Last step lead to the first one (periodicity)
-elements_before = sorted_list_of_possible_types[len(sorted_list_of_possible_types)-1]
-elements_after = sorted_list_of_possible_types[0]
-#print elements_before
-#print elements_after
-
-coordinates = [[   0.000000e+00,   0.000000e+00,   0.000000e+00]]
-processes.append(KMCProcess(coordinates=coordinates,
-                                       elements_before=[elements_before],
-                                       elements_after=[elements_after],
-                                       basis_sites=[0],
-                                       rate_constant=0.0))
-
-before_moving = [elements_after, elements_before]
-after_moving = [elements_before, elements_after]
+# Last steps lead to the first ones (periodicity) (2 last steps linked to the 2 first)
+for i in range(1,3):
     
-for i in range(len(list_of_coordinates)):
-    processes.append(KMCProcess(coordinates=list_of_coordinates[i],
-                                           elements_before=before_moving,
-                                           elements_after=after_moving,
+    elements_before = sorted_list_of_possible_types[len(sorted_list_of_possible_types)-(3-i)]
+    elements_after = sorted_list_of_possible_types[i]
+    #print elements_before
+    #print elements_after
+    
+    coordinates = [[   0.000000e+00,   0.000000e+00,   0.000000e+00]]
+    processes.append(KMCProcess(coordinates=coordinates,
+                                           elements_before=[elements_before],
+                                           elements_after=[elements_after],
                                            basis_sites=[0],
                                            rate_constant=0.0))
+    
+    step_jump = sorted_list_of_possible_types[i+1]
+    
+    before_moving = [elements_after, elements_before]
+    after_moving = [elements_before, elements_after]
+    step_jumping = [elements_before, step_jump]
+        
+    for j in range(len(list_of_coordinates)):
+        processes.append(KMCProcess(coordinates=list_of_coordinates[j],
+                                               elements_before=before_moving,
+                                               elements_after=after_moving,
+                                               basis_sites=[0],
+                                               rate_constant=0.0))
+    for j in range(len(list_of_coordinates)):
+        processes.append(KMCProcess(coordinates=list_of_coordinates[j],
+                                               elements_before=before_moving,
+                                               elements_after=step_jumping,
+                                               basis_sites=[0],
+                                               rate_constant=0.0))
 
 #print len(processes)
 # Create the interactions object.
--- KMC_Spyder/SiGrowth/Travail sur les évènements/run_test_4_steps_A_B.py
+++ KMC_Spyder/SiGrowth/Travail sur les évènements/run_test_4_steps_A_B.py
@@ -15,85 +15,82 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
     def rate(self, geometry, elements_before, elements_after, rate_constant, process_number, coordinate):
         """ Overloaded base class API function """
         
-    	# # Physical values
-    	# T = 850 #temperature
-    	# kb = 1.38*10**(-23)
-    	# q = 1.6*10**(-19)
-    	# E_substrate = 1.3
-    	# E_normal = 0.05
-    	# E_parallel = 0.5
-    	# k0 = 10**13
+    	 # Physical values
+    	 T = 850 #temperature
+    	 kb = 1.38*10**(-23)
+    	 q = 1.6*10**(-19)
+    	 E_substrate = 1.3
+    	 E_normal = 0.05
+    	 E_parallel = 0.5
+    	 k0 = 10**13
     
-    	# SendFlux = 0
+    	 SendFlux = 0
 
-     #    n_parallel = 0
-     #    n_normal = 0        
+         n_parallel = 0
+         n_normal = 0        
 
-    	# #print element_before[0]    
-     #    concerned_dimere = elements_before[0]
-     #    dimere_type = concerned_dimere[0]
+    	 #print element_before[0]    
+         concerned_dimere = elements_before[0]
+         dimere_type = concerned_dimere[0]
         
-     #    # Processe's order = [add dimere, go right, go left, go forward, go backward]
-     #    # Repeat X times where X is the number of different steps
+         # Processe's order = [add dimere, go right, go left, go forward, go backward]
+         # Repeat X times where X is the number of different steps
         
-     #    #to avoid vacancies diffusion in an higher step
-     #    is_in_bulk = 0
-     #    for i in range(1, 4+1):
-     #        if elements_before[0] == elements_before[i]:
-     #            is_in_bulk += 1
+         #to avoid vacancies diffusion in an higher step
+         is_in_bulk = 0
+         for i in range(1, 4+1):
+             if elements_before[0] == elements_before[i]:
+                 is_in_bulk += 1
         
-     #    # Add a dimere on top case
-     #    if process_number % 5 == 0:
-     #        if is_in_bulk == 4:
-     #            return SendFlux
-     #        else:
-     #            return 0
+         # Add a dimere on top case
+         if process_number % 9 == 0:
+             if is_in_bulk == 4:
+                 return SendFlux
+             else:
+                 return 0
         
-     #    if is_in_bulk >= 3 and (process_number % 5 != 0):
-     #        return 0
+         if is_in_bulk >= 3 and (process_number % 9 != 0):
+             return 0
         
-     #    # if process_number % 5 != 0:
-     #    #     return 0
+         # if process_number % 5 != 0:
+         #     return 0
   
-     #    else:
-     #        Move_A = (process_number % 5 != 0) and (dimere_type == 'A')
-     #        Move_B = (process_number % 5 != 0) and (dimere_type == 'B')
+         else:
+             Move_A = (process_number % 9 != 0) and (dimere_type == 'A')
+             Move_B = (process_number % 9 != 0) and (dimere_type == 'B')
             
-     #        ###################################
-     #        # Anisotropy is implemented here !#
-     #        ###################################
+             ###################################
+             # Anisotropy is implemented here !#
+             ###################################
             
-     #        if Move_A:
+             if Move_A:
                 
-     #            if dimere_type == elements_before[2]:
-     #                n_parallel += 1
-     #            if dimere_type == elements_before[3]:
-     #                n_parallel += 1
-     #            if dimere_type == elements_before[1]:
-     #                n_normal += 1
-     #            if dimere_type == elements_before[4]:
-     #                n_normal += 1
+                 if concerned_dimere == elements_before[2]:
+                     n_parallel += 1
+                 if concerned_dimere == elements_before[3]:
+                     n_parallel += 1
+                 if concerned_dimere == elements_before[1]:
+                     n_normal += 1
+                 if concerned_dimere == elements_before[4]:
+                     n_normal += 1
                 
-     #            E_tot = E_substrate + n_normal * E_normal + n_parallel * E_parallel
-     #            return k0*np.exp( - E_tot * q / (kb * T) )
+                 E_tot = E_substrate + n_normal * E_normal + n_parallel * E_parallel
+                 return k0*np.exp( - E_tot * q / (kb * T) )
 
             
-     #        if Move_B:
+             if Move_B:
                 
-     #            if dimere_type == elements_before[2]:
-     #                n_normal +=1
-     #            if dimere_type == elements_before[3]:
-     #                n_normal +=1
-     #            if dimere_type == elements_before[1]:
-     #                n_parallel += 1
-     #            if dimere_type == elements_before[4]:
-     #                n_parallel += 1
+                 if concerned_dimere == elements_before[2]:
+                     n_normal +=1
+                 if concerned_dimere == elements_before[3]:
+                     n_normal +=1
+                 if concerned_dimere == elements_before[1]:
+                     n_parallel += 1
+                 if concerned_dimere == elements_before[4]:
+                     n_parallel += 1
                 
-     #            E_tot = E_substrate + n_normal * E_normal + n_parallel * E_parallel
-     #            return k0*np.exp( - E_tot * q / (kb * T) )
-     
-        return 0.0
-
+                 E_tot = E_substrate + n_normal * E_normal + n_parallel * E_parallel
+                 return k0*np.exp( - E_tot * q / (kb * T) )
         
     def cutoff(self):
         """ Determines the cutoff for this custom model """
