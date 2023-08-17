e4d77786dc524d4aa21bf9196cbf46323793591b
MSilvestre-Growth <141043947+MSilvestre-Growth@users.noreply.github.com>
2023-08-17 09:18:39 +0200

    pas de rebouclage

---
+++ KMC_Spyder/SiGrowth/Pas de rebouclage/custom_processes_An_Bn_bon_niveaux.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 16:52:10 2023
+
+@author: msilvestre
+
+We add a virtual step on top of the highest one to simulate the highest step burrying
+"""
+
+# Copyright (c)  2014  Mikael Leetmaa
+#
+# This file is part of the KMCLib project distributed under the terms of the
+# GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
+#
+
+
+from KMCLib import *
+
+# List to store all processes
+processes = []
+
+# store the configuration to get all possibles types
+config = KMCConfigurationFromScript("config_4_steps.py")
+
+
+# Get the possible types from config
+dictionnary_of_possible_types = config.possibleTypes()
+#print dictionnary_of_possible_types
+# Removal of generic '*' type from the dictionnary
+del dictionnary_of_possible_types['*']
+# Conversion of a dictionnary in the list of all entries
+list_of_possible_types = dictionnary_of_possible_types.keys()
+#print list_of_possible_types
+
+sorted_list_of_possible_types = []
+NumberOfTypes = len(list_of_possible_types)
+
+# Get min height to sort list_of_possible_types
+
+for i in range(NumberOfTypes):
+    #print list_of_possible_types 
+    min_height = int(list_of_possible_types[0][1])
+    index = 0
+    for j in range(len(list_of_possible_types)):
+        current_height = int(list_of_possible_types[j][1])
+        if current_height < min_height:
+            min_height = current_height
+            index = j
+    sorted_list_of_possible_types.append(list_of_possible_types[index])
+    del list_of_possible_types[index]
+
+# sorted_list_of_possible_types --> list sorted from the lowest to the highest step
+
+# List of all possible deplacement in 2 dimensions
+    
+# From elements_before point of view : list_of_coordinates = [forward, left, right, backward]
+# From elements_after point of view : list_of_coordinates = [left, right, backward, forward]
+# THos move order is due to elements_before order in the custom rate calculator in the run programm
+list_of_coordinates = [[[0.0, 0.0, 0.0], [0.0, 1.0, 0.0]],
+                       [[0.0, 0.0, 0.0], [-1.0, 0.0, 0.0]],
+                       [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0]],
+                       [[0.0, 0.0, 0.0], [0.0, -1.0, 0.0]]
+                       ]
+
+for a in range(len(sorted_list_of_possible_types)-2):
+
+    #################################################
+    #        Deposition of a quasi-dimere           #
+    #################################################
+    
+    elements_before = sorted_list_of_possible_types[a]
+    #print 'elements_before'
+    #print elements_before
+    
+    elements_after = sorted_list_of_possible_types[a+1]
+    #print 'elements_after'
+    #print elements_after
+    
+    # All steps but the last one
+    coordinates = [[   0.000000e+00,   0.000000e+00,   0.000000e+00]]
+    processes.append(KMCProcess(coordinates=coordinates,
+                                           elements_before=[elements_before],
+                                           elements_after=[elements_after],
+                                           basis_sites=[0],
+                                           rate_constant=0.0))
+
+    #################################################
+    #         Diffusion of a quasi-dimere           #
+    #################################################
+    step_jump = sorted_list_of_possible_types[a+2]
+    
+    before_moving = [elements_after, elements_before]
+    #print 'before_moving'
+    #print before_moving
+    
+    after_moving = [elements_before, elements_after]
+    #print 'after_moving'
+    #print after_moving
+    
+    before_jump = [elements_before, step_jump]
+    step_jumping = [elements_after, elements_after]
+    
+    # print "elements_before"
+    # print elements_before
+       
+    # print "elements_after"
+    # print elements_after
+
+    # print "step_jump"
+    # print step_jump
+    
+    for i in range(len(list_of_coordinates)):
+        processes.append(KMCProcess(coordinates=list_of_coordinates[i],
+                                               elements_before=before_moving,
+                                               elements_after=after_moving,
+                                               basis_sites=[0],
+                                               rate_constant=0.0))
+    for i in range(len(list_of_coordinates)):   
+        processes.append(KMCProcess(coordinates=list_of_coordinates[i],
+                                               elements_before=before_jump,
+                                               elements_after=step_jumping,
+                                               basis_sites=[0],
+                                               rate_constant=0.0))
+
+#print len(processes)
+# Create the interactions object.
+interactions = KMCInteractions(processes, implicit_wildcards=True)
---
+++ KMC_Spyder/SiGrowth/Pas de rebouclage/generate_config_4_steps.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 14:36:22 2023
+
+@author: msilvestre
+"""
+
+# Copyright (c)  2014  Mikael Leetmaa
+#
+# This file is part of the KMCLib project distributed under the terms of the
+# GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
+#
+
+from KMCLib import *
+
+# ----------------------------------------------------------------------------
+# Unit cell
+
+cell_vectors = [[   1.000000e+00,   0.000000e+00,   0.000000e+00],
+                [   0.000000e+00,   1.000000e+00,   0.000000e+00],
+                [   0.000000e+00,   0.000000e+00,   1.000000e+00]]
+
+basis_points = [[   0.000000e+00,   0.000000e+00,   0.000000e+00]]
+
+unit_cell = KMCUnitCell(
+    cell_vectors=cell_vectors,
+    basis_points=basis_points)
+
+# -----------------------------------------------------------------------------
+# Lattice
+
+lattice = KMCLattice(
+    unit_cell=unit_cell,
+    repetitions=(100,100,1),
+    periodic=(True, True, False))
+
+# -----------------------------------------------------------------------------
+# Configuration
+
+types = ['A1']*2500
+
+for i in range(2500) :
+    types.append('B2')
+    
+for j in range(2500) :
+    types.append('A3')
+
+for k in range(2500) :
+    types.append('B4')
+
+possible_types = ['A1','B2','A3','B4']
+sorted_step = []
+
+for i in range(len(possible_types)):
+    #print possible_types 
+    min_height = int(possible_types[0][1])
+    index = 0
+    for j in range(len(possible_types)):
+        current_height = int(possible_types[j][1])
+        if current_height < min_height:
+            min_height = current_height
+            index = j
+    sorted_step.append(possible_types[index])
+    del possible_types[index]
+
+last_step =  sorted_step(len(sorted_step)-1)
+last_step_type = last_step[0]
+last_step_height = int(last_step[1])
+
+# Add a new virtual highest step in the list
+if last_step_type == "A":
+    new_high_step = "B" + str(last_step_height+1)
+    sorted_step.append(new_high_step)
+if last_step_type == "B":
+    new_high_step = "A" + str(last_step_height+1)
+    sorted_step.append(new_high_step)
+    
+configuration = KMCConfiguration(
+    lattice=lattice,
+    types=types,
+    possible_types=sorted_step)
+
+# print tests
+dictionnary_of_possible_types = config.possibleTypes()
+print dictionnary_of_possible_types
+
+list_of_possible_types = dictionnary_of_possible_types.keys()
+del dictionnary_of_possible_types['*']
+print list_of_possible_types
+# end print tests
+
+# Use the _script() function to get a script that can generate the configuration.
+print "from KMCLib import *"
+print configuration._script()
