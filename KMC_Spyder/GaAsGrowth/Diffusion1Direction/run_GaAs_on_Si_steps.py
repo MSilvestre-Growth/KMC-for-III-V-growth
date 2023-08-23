ef758a294f35a08ee17f4019c22f0fc0359f85b5
SILVESTRE Milan <milansilvestre@umontpellier.fr>
2023-08-23 17:46:48 +0200

    reluts

--- KMC_Spyder/GaAsGrowth/Diffusion1Direction/run_GaAs_on_Si_steps.py
+++ KMC_Spyder/GaAsGrowth/Diffusion1Direction/run_GaAs_on_Si_steps.py
@@ -62,7 +62,7 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
         E_parallel = 0.5
         k0 = 10**13 #hopping constant for the Boltzman's law
     
-        SendFlux = 0.37
+        SendFlux =10 
 
         n_parallel = 0
         n_normal = 0        
