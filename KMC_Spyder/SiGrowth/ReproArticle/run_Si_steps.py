44ed67447583f0d08c25d47716739f9da06e5d75
SILVESTRE Milan <milansilvestre@umontpellier.fr>
2023-08-24 23:47:27 +0200

    result

--- KMC_Spyder/SiGrowth/ReproArticle/run_Si_steps.py
+++ KMC_Spyder/SiGrowth/ReproArticle/run_Si_steps.py
@@ -22,9 +22,9 @@ there is no stepflow above it
 from KMCLib import *
 import numpy as np
 import time
-from KMCLib.KMCAnalysisPlugin import KMCAnalysisPlugin
+#from KMCLib.KMCAnalysisPlugin import KMCAnalysisPlugin
 
-Nb_sent_atoms = 0
+
 # Set the custom rate --> all the physics is here !!!
 class CustomRateCalculator(KMCRateCalculatorPlugin):
     """ Class for defining the custom rates function for the KMCLib paper. """
@@ -34,7 +34,7 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
         """ Overloaded base class API function """
         
         # Physical value
-        T = 1000 #temperature
+        T = 950 #temperature
         kb = 1.38*10**(-23)
         q = 1.6*10**(-19)
         E_substrate = 1.3
@@ -49,7 +49,7 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
 
         Nb_processes_per_type = 5        
 
-    	global Nb_sent_atoms
+    
 
     	#print element_before[0]    
         concerned_dimere = elements_before[0]
@@ -69,7 +69,7 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
         # Add a dimere on top case
         if process_number % Nb_processes_per_type == 0:
             if is_in_bulk == 4:
-                Nb_sent_atoms += 1
+                
 		return SendFlux
             else:
                 return 0
@@ -118,29 +118,29 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
         """ Determines the cutoff for this custom model """
         return 1.0
 
-class CustomAnalysis(KMCAnalysisPlugin):
-    """ Custom analysis class """
-    def __init__(self, init_config, current_config):
-        self.init_config = init_config
-        self.curent_config = current_config
+#class CustomAnalysis(KMCAnalysisPlugin):
+#    """ Custom analysis class """
+#    def __init__(self, init_config, current_config):
+#        self.init_config = init_config
+#        self.curent_config = current_config
 	
-    def setup(self, step, time, configuration):
-        if step == 0:
-            fig0 = configuration.types()
-        fig_now = configuration.types()
-        p = CustomAnalysis(config0, config_now)
-        return comp
+#    def setup(self, step, time, configuration):
+#        if step == 0:
+#            fig0 = configuration.types()
+#        fig_now = configuration.types()
+#        p = CustomAnalysis(config0, config_now)
+#        return comp
     
-    def finalize(self):
-        p = setup(step, time, configuration)
-        t_types = comp.init_config()
-        rent_types = comp.current_config()
-        t_atoms = 0
-        i in range(len(init_types)):
-             sent_atoms += int(current_types[i][1]) - int(init_types[i][1])
-        return sent_atoms/(len(init_types)*time)
+#    def finalize(self):
+#        p = setup(step, time, configuration)
+#        t_types = comp.init_config()
+#        rent_types = comp.current_config()
+#        t_atoms = 0
+#        i in range(len(init_types)):
+#             sent_atoms += int(current_types[i][1]) - int(init_types[i][1])
+#        return sent_atoms/(len(init_types)*time)
 
-analysis = CustomAnalysis.finalize()
+#analysis = CustomAnalysis.finalize()
 
 # speedup process
 def TrueFuction(obj):
@@ -166,15 +166,14 @@ model = KMCLatticeModel(configuration=config,
 # so we would expect slightly different results each time
 # we run this test.
 control_parameters = KMCControlParameters(number_of_steps=1000000,
-                                          dump_interval=100000,
-                                          analysis_interval = 100000,
+                                          dump_interval=100000, 
                                           seed=120)
 t1 = time.clock()
-model.run(control_parameters, trajectory_filename="custom_traj_4_steps.py", analysis = analysis)
+model.run(control_parameters, trajectory_filename="custom_traj_4_steps.py")
 t2 = time.clock()
 
 print "simu time = "
 print t2 - t1
 
-print "Nb sent atoms"
-print Nb_sent_atoms
+
+
