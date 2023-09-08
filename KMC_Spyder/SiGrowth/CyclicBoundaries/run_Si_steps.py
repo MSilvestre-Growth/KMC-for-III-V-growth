24e6a228928e1a9641201142beb660e7337732eb
SILVESTRE Milan <milansilvestre@umontpellier.fr>
2023-09-08 11:33:52 +0200

    test

--- KMC_Spyder/SiGrowth/CyclicBoundaries/run_Si_steps.py
+++ KMC_Spyder/SiGrowth/CyclicBoundaries/run_Si_steps.py
@@ -82,8 +82,8 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
         if is_in_bulk >= 3 and process_number % Nb_processes_per_type > 1 :
             return 0
  
-	if process_number % Nb_processes_per_type in [2,8] :
-	    return 0
+	    if process_number % Nb_processes_per_type == 2 or process_number % Nb_process_per_type == 8 :
+	        return 0
  
         if is_in_bulk < 3 and process_number % Nb_processes_per_type > 1 :
 	    print process_number
