c9de217d062a6adc3ff60aa6ada33bee823d3f05
MSilvestre-Growth <141043947+MSilvestre-Growth@users.noreply.github.com>
2023-09-08 18:35:44 +0200

    fix processes

--- KMC_Spyder/SiGrowth/CyclicBoundaries/custom_processes.py
+++ KMC_Spyder/SiGrowth/CyclicBoundaries/custom_processes.py
@@ -183,6 +183,12 @@ while a < len(sorted_list_of_possible_types)-3:
                                                elements_after=[elements_before,elements_after_interface],
                                                basis_sites=[0],
                                                rate_constant=0.0))
+        
+        processes.append(KMCProcess(coordinates=list_of_coordinates[i],
+                                               elements_before=[elements_after_interface,elements_before],
+                                               elements_after=[elements_before_interface,elements_after],
+                                               basis_sites=[0],
+                                               rate_constant=0.0))
         processes_name_list.append("Interface process, before : " + elements_after +' --> '+ elements_before_interface + ' after : '+ elements_before +' --> '+ elements_after_interface)
     a += 2
 
