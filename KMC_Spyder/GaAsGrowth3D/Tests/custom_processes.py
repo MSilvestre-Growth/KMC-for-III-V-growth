1385fed83039440465a788f82a6e173a52316b53
MSilvestre-Growth <141043947+MSilvestre-Growth@users.noreply.github.com>
2024-01-05 13:25:24 +0100

    diffusion of i_GaAs on Si

--- KMC_Spyder/GaAsGrowth3D/Tests/custom_processes.py
+++ KMC_Spyder/GaAsGrowth3D/Tests/custom_processes.py
@@ -360,7 +360,7 @@ for j in range(len(i_Si_type)):
         
         # move forward = cycling step
         elements_before.append([i_GaAs_type[j], "V", cycling_step_Si_type[j], "V"])
-        elements_after.append(["V", i_Si_type[j], cycling_step_GaAs_type[j], "V"])
+        elements_after.append(["V", GaAs_type[j], cycling_step_Si_type[j], "V"])
         
         # move backward = i_GaAs transforms into GaAs
         elements_before.append([i_GaAs_type[j], "V", Si_type[j], "V"])
