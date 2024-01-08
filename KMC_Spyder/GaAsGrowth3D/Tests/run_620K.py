141f7c1b5ad61257556aa14337a9f754cdd751ee
SILVESTRE Milan <milan.silvestre@umontpellier.fr>
2024-01-08 13:41:13 +0100

    test

--- KMC_Spyder/GaAsGrowth3D/Tests/run_620K.py
+++ KMC_Spyder/GaAsGrowth3D/Tests/run_620K.py
@@ -104,7 +104,7 @@ model = KMCLatticeModel(configuration=config,
 # so we would expect slightly different results each time
 # we run this test.
 
-number_of_steps=2
+number_of_steps=30
 
 control_parameters = KMCControlParameters(number_of_steps=number_of_steps,
                                           dump_interval=1,
