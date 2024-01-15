fc61dc774ef250b00b8a1e2df08d00719d67c405
MSilvestre-Growth <141043947+MSilvestre-Growth@users.noreply.github.com>
2024-01-15 10:17:45 +0100

    test

--- KMC_Spyder/GaAsGrowth3D/Test_Si_2D/generate_config_Si2D_forGaAs_3D.py
+++ KMC_Spyder/GaAsGrowth3D/Test_Si_2D/generate_config_Si2D_forGaAs_3D.py
@@ -40,7 +40,7 @@ unit_cell = KMCUnitCell(
 
 # Precise the number of pixel in each direction (at least 1)
 X = 80
-Y = 10
+Y = 30
 Z = 10
 
 # Precise if you want your structure to be periodic in different directions
