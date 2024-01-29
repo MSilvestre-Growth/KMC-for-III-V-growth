15fa8d3ccb8cc5de7745700d3c5d8c694f47e23d
SILVESTRE Milan <milan.silvestre@umontpellier.fr>
2024-01-29 14:12:52 +0100

    test

--- KMC_Spyder/GaAsGrowth3D/Tests/run_620K.py
+++ KMC_Spyder/GaAsGrowth3D/Tests/run_620K.py
@@ -60,7 +60,7 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
 
         global SendFlux
         
-        if 0<=process_number<=7 and process_number != 6:
+        if 0<=process_number<=7 and process_number != 4:
             return 0.0
         else:
             return 1.0
