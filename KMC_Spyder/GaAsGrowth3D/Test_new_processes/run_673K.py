64d63ef74e4fa3e6effb0fe82e58e05b0afb9f40
SILVESTRE Milan <milan.silvestre@umontpellier.fr>
2024-02-20 10:20:41 +0100

    test

--- KMC_Spyder/GaAsGrowth3D/Test_new_processes/run_673K.py
+++ KMC_Spyder/GaAsGrowth3D/Test_new_processes/run_673K.py
@@ -115,11 +115,12 @@ class CustomRateCalculator(KMCRateCalculatorPlugin):
             if ((process_number-100) % 4 == 0) :#and elements_before[41] == "V": 
                 return 0 
             if ((process_number-100) % 4 == 1) :#and elements_before[41] == "V":
-                # X + 1 // 41
+                # 41
 		return 0
-            if ((process_number-100) % 4 == 2) :#and elements_before[46] == "V":
+            if ((process_number-100) % 4 == 2) and elements_before[46] == "V":
                 return 0
-            if ((process_number-100) % 4 == 3) and elements_before[37] == "V":
+            if ((process_number-100) % 4 == 3) :#and elements_before[37] == "V":
+		# 37
                 return 0
             else:
                 return k0
